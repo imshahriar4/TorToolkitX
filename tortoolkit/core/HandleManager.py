57f602bcaa3aeb3c21554351a222c2711f272ee0
P R I Y O <84788291+itspriyo@users.noreply.github.com>
2021-10-16 10:20:25 +0600

    Update HandleManager.py

--- tortoolkit/core/HandleManager.py
+++ tortoolkit/core/HandleManager.py
@@ -644,7 +644,7 @@ async def set_password_zip(message):
 
 
 async def start_handler(event):
-    msg = "Hello This is TorToolkitX running on heroku an instance of <a href='https://github.com/XcodersHub/TorToolkitX'>This Repo</a>. Try the repo for yourself and dont forget to put a STAR and fork."
+    msg = "Hello This is modified TorToolkitX running on heroku an instance of <a href='https://github.com/itspriyo/TorToolkitX'>This Repo</a>. Try the repo for yourself and dont forget to put a STAR and fork."
     await event.reply(msg, parse_mode="html")
 
 
